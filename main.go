be381b071180f62b494a9c30746daa94d6b28b98
alifma <alifmaulana26@gmail.com>
2021-03-27 15:04:29 +0700

    ajax response

--- main.go
+++ main.go
@@ -1,79 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"path/filepath"
-	"text/template"
 )
 
-func routeIndexGet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "", http.StatusBadRequest)
-		return
-	}
-
-	var tmpl = template.Must(template.ParseFiles("view.html"))
-	var err = tmpl.Execute(w, nil)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "", http.StatusBadRequest)
-		return
-	}
-
-	if err := r.ParseMultipartForm(1024); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	alias := r.FormValue("alias")
-
-	uploadedFile, handler, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer uploadedFile.Close()
-
-	dir, err := os.Getwd()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	filename := handler.Filename
-	if alias != "" {
-		filename = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
-	}
-	fileLocation := filepath.Join(dir, "files", filename)
-	targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer targetFile.Close()
-
-	if _, err := io.Copy(targetFile, uploadedFile); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write([]byte("done"))
-}
-
 func main() {
-	http.HandleFunc("/", routeIndexGet)
-	http.HandleFunc("/process", routeSubmitPost)
+	http.HandleFunc("/", ActionIndex)
 
-	var addr = ":9000"
-	fmt.Printf("Service are started on localhost%s", addr)
-	http.ListenAndServe(addr, nil)
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func ActionIndex(w http.ResponseWriter, r *http.Request) {
+	data := []struct {
+		Name string
+		Age  int
+	}{
+		{"Richard Grayson", 24},
+		{"Jason Todd", 23},
+		{"Tim Drake", 22},
+		{"Damian Wayne", 21},
+	}
+
+	jsonInBytes, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonInBytes)
 }
